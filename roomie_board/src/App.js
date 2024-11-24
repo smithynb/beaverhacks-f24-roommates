67df04736b3260413299b0ccb77860c020368004
smithynb <smithynb@oregonstate.edu>
2024-11-23 17:00:10 -0800

    basic page

--- roomie_board/src/App.js
+++ roomie_board/src/App.js
@@ -3,15 +3,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className = "text-3xl font-bold font-sans text-center text-green-400">Roomie Board</h1>
-        <p className = "text-left font-sans">Your all-in-one chores app. Made for students by students.</p>
-        <h2 className = "font-sans">Calendar and todo list here</h2>
-      </header>
-      
-      <body className = "App-body">
-      </body>
-      
+
+      <body className = "App-body font-sans">
+        <div className = "roommate-container">
+          <select name = "roommate" id = "select-roommate">
+            <option value = "roommate1">Louis'</option>
+            <option value = "roommate2">Benny's</option>
+            <option value = "roommate3">Reina's</option>
+          </select>
+          <h1 className = "font-sans">RoomieBoard</h1>
+        </div>
+        
+
+        <div className = "time-view-container">
+            <h2 className = "flex">Time View</h2>
+            <button className = "flex">Day</button>
+            <button className = "flex">Week</button>
+          </div>
+
+          <div className = "calendar-container">
+
+          </div>
+
+          <div className = "task-container">
+            <h2>Tasks</h2>
+            <button>Add Task</button>
+          </div>
+
+          <div className = "todo-board-container">
+            <h2>To-do</h2>
+            <label>
+            <input type = "checkbox"/>Clean the stove</label>
+          </div>
+
+
+        </body>
+
     </div>
   );
 }
