a4b483b39ee7c4311426b739696d16e3f9da0bce
Reina128 <takaharr@oregonstate.edu>
2024-11-23 23:06:33 -0800

    calendar functionality

--- roomie_board/src/components/Calendar.js
+++ roomie_board/src/components/Calendar.js
@@ -1,13 +1,28 @@
 
 
 import React from 'react';
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-const Calendar = () => {
+
+function Calendar() {
   return (
-    <div className = "calendar-container">
-            <h2>Calendar goes here</h2>
-        </div>
+    <div className="App">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        headerToolbar={{
+          center: "dayGridMonth,timeGridWeek,timeGridDay new",
+        }}
+        eventColor="red"
+        nowIndicator
+        dateClick={(e) => console.log(e.dateStr)}
+        eventClick={(e) => console.log(e.event.id)}
+      />
+    </div>
   );
-};
+}
 
-export default Calendar;   
+export default Calendar;
